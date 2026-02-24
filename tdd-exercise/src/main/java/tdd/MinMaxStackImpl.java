25d8bb752aeebc2d78e13efb75c4aa4a90347540
Eduard Toni Alexandru <eduardtoni.alexandru@studio.unibo.it>
2026-02-24 11:56:27 +0100

    feat: add test for push and peek

---
+++ tdd-exercise/src/main/java/tdd/MinMaxStackImpl.java
@@ -0,0 +1,42 @@
+package tdd;
+
+import java.util.Stack;
+
+public class MinMaxStackImpl implements MinMaxStack {
+    private final Stack<Integer> stack = new Stack<>();
+
+    @Override
+    public void push(int value) {
+        stack.push(value);
+    }
+
+    @Override
+    public int pop() {
+        return 0;
+    }
+
+    @Override
+    public int peek() {
+        return stack.peek();
+    }
+
+    @Override
+    public int getMin() {
+        return 0;
+    }
+
+    @Override
+    public int getMax() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+}
--- tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
+++ tdd-exercise/src/test/java/tdd/MinMaxStackImplTest.java
@@ -1,12 +1,23 @@
 package tdd;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MinMaxStackImplTest {
+    private MinMaxStack stack;
+
+    @BeforeEach
+    void setUp() {
+        stack = new MinMaxStackImpl();
+    }
+
     @Test
-    public void todo() {
-        assertTrue(true);
+    public void pushShouldAddAnElementOnTop() {
+        final var elementToAdd = 1;
+        stack.push(1);
+        Assertions.assertEquals(elementToAdd, stack.peek());
     }
 }
